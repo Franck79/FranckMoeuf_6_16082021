d4195261cb5b4f8c96fe8bda0104af3743fdcc0d
Franck <fmoeuf@gmail.com>
2021-09-28 13:52:20 +0200

    update babel

--- src/query.js
+++ src/query.js
@@ -1,6 +1,6 @@
 
 export default async function retrieveContent() {
-    const url = "https://baconipsum.com/api/?type=all-meat&paras=2&start-with-lorem=1";
+    const url = "https://franck79.github.io/FranckMoeuf_6_16082021/";
   
     const response = await fetch(url);
     return response.json();
