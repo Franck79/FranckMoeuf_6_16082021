69e821cb3bc135a511573b7bf60d69d486ee21b7
Franck <fmoeuf@gmail.com>
2021-09-28 14:17:44 +0200

    test build

--- src/query.js
+++
@@ -1,8 +0,0 @@
-
-export default async function retrieveContent() {
-    const url = "https://franck79.github.io/FranckMoeuf_6_16082021/";
-  
-    const response = await fetch(url);
-    return response.json();
-  }
-  
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   mode: "production",
   entry: {
     polyfill: "babel-polyfill",
-    app: "./src/index.js"
+    app: "./js/index.js",
   },
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
+    sourceMapFilename: "[name].js.map",
   },
   module: {
     rules: [
@@ -18,10 +19,15 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
-      }
-    ]
-  }
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
+      },
+    ],
+  },
+  devtool: "source-map",
+  devServer: {
+    writeToDisk: true,
+  },
 };
